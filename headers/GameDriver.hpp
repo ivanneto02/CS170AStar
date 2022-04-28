2cac966a2f61eafb897a5dd0050e8ea049197d0d
ivanneto02 <ivan.a.neto@hotmail.com>
2022-04-28 07:49:29 -0700

    Filled in header for GameDriver class

--- headers/GameDriver.hpp
+++ headers/GameDriver.hpp
@@ -1,14 +1,21 @@
 #ifndef __GAMEDRIVER_HPP__
 #define __GAMEDRIVER_HPP__
 
+// Currently just using C++ STD Queue
+#include <queue>
+#include <set>
+#include "Board.hpp"
+#include "Problem.hpp"
+
 class GameDriver {
 
     private:
-        
+        std::queue<Board*> frontier;
+        std::set<Board*> explored;
 
     public:
 
-
+        Board* graphSearch(Problem*);
 };
 
 #endif
